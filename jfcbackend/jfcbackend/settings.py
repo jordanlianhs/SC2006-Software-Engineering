d603cacc55b33c96071447f3619d09fe62b343fb
Han <tanjinghan21@gmail.com>
2023-03-15 17:16:44 +0800

    priceprediction app for database

--- jfcbackend/jfcbackend/settings.py
+++ jfcbackend/jfcbackend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'pricePrediction.apps.PricepredictionConfig',
+
     'api.apps.ApiConfig',
     'rest_framework',
     'crispy_forms',
---
+++ jfcbackend/pricePrediction/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import gettext as _
+import requests
+
+
+#import urllib.request
+
+link = "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&limit=5"
+test = requests.get(link).json()
+print(test)
+
+class HousePrices(models.Model):
+    month = models.DateField(_('month'), max_length=255)
+    town = models.CharField(_('town'), max_length=255)
+
+# with urllib.request.urlopen(link) as url:
+#     fileobj = urllib.urlopen(url)
+#     print(fileobj.read())
+
+# Create your models here.
