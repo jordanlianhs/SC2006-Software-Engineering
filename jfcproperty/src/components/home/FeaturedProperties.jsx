d67bc4c50f7ecad99b44327f2728b8892b6546f6
Xiaoyany88 <104213111+Xiaoyany88@users.noreply.github.com>
2023-03-16 00:39:29 +0800

    changed each item in recommended properties to display only specified info

--- jfcproperty/src/components/home/FeaturedProperties.jsx
+++ jfcproperty/src/components/home/FeaturedProperties.jsx
@@ -34,21 +34,8 @@ const FeaturedProperties = () => {
         <div className="thumb">
           <img className="img-whp" src={item.img} alt="fp1.jpg" />
           <div className="thmb_cntnt">
-            <ul className="tag mb0">
-              {item.saleTag.map((val, i) => (
-                <li className="list-inline-item" key={i}>
-                  <a href="#">{val}</a>
-                </li>
-              ))}
-            </ul>
-            {/* End .tag */}
 
             <ul className="icon mb0">
-              <li className="list-inline-item">
-                <a href="#">
-                  <span className="flaticon-transfer-1"></span>
-                </a>
-              </li>
               <li className="list-inline-item">
                 <a href="#">
                   <span className="flaticon-heart"></span>
@@ -59,8 +46,7 @@ const FeaturedProperties = () => {
 
             <Link href={`/listing-details-v1/${item.id}`}>
               <a className="fp_price">
-                ${item.price}
-                <small>/mo</small>
+                ${item.resalePrice}
               </a>
             </Link>
           </div>
@@ -72,44 +58,17 @@ const FeaturedProperties = () => {
             <p className="text-thm">{item.type}</p>
             <h4>
               <Link href={`/listing-details-v1/${item.id}`}>
-                <a>{item.title}</a>
+                <a>{item.blockNumber}</a>
               </Link>
             </h4>
             <p>
               <span className="flaticon-placeholder"></span>
-              {item.location}
+              {item.StreetName}
             </p>
 
-            <ul className="prop_details mb0">
-              {item.itemDetails.map((val, i) => (
-                <li className="list-inline-item" key={i}>
-                  <a href="#">
-                    {val.name}: {val.number}
-                  </a>
-                </li>
-              ))}
-            </ul>
           </div>
           {/* End .tc_content */}
-
-          <div className="fp_footer">
-            <ul className="fp_meta float-start mb0">
-              <li className="list-inline-item">
-                <Link href="/agent-v2">
-                  <a>
-                    <img src={item.posterAvatar} alt="pposter1.png" />
-                  </a>
-                </Link>
-              </li>
-              <li className="list-inline-item">
-                <Link href="/agent-v2">
-                  <a>{item.posterName}</a>
-                </Link>
-              </li>
-            </ul>
-            <div className="fp_pdate float-end">{item.postedYear}</div>
-          </div>
-          {/* End .fp_footer */}
+ 
         </div>
         {/* End .details */}
       </div>
