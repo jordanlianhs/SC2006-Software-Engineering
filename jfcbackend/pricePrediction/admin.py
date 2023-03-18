ffc1e573222c8585478e77fea175284bb69ae529
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-03-18 21:48:45 +0800

    Recent changes

--- jfcbackend/pricePrediction/admin.py
+++ jfcbackend/pricePrediction/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import HousePrice
 # Register your models here.
+
+admin.site.register(HousePrice)
--- jfcbackend/pricePrediction/models.py
+++ jfcbackend/pricePrediction/models.py
@@ -9,10 +9,25 @@ import requests
 #test = requests.get(link).json()
 #print(test)
 
-class HousePrices(models.Model):
-    #month = models.CharField(_('month'), max_length=255)
-    town = models.CharField(_('town'), max_length=255)
+class HousePrice(models.Model):
+    #Should create enum for town, flat_type, flat_model, storey_range
 
+    id = models.IntegerField(_('id'), primary_key=True)
+    month = models.CharField(_('month'), max_length=10, )
+    town = models.CharField(_('town'), max_length=255)
+    flat_type = models.CharField(_("flat_type"), max_length=50)
+    block  = models.IntegerField(_("block"), blank=True)
+    street_name = models.CharField(_("street_name"), max_length=255)
+    storey_range = models.CharField(_("storey_range"), max_length=8)
+    floor_area_sqm = models.IntegerField(_("floor_area_sqm"))
+    flat_model = models.CharField(_("flat_model"), max_length=255)
+    lease_commence_date = models.IntegerField(_("lease_commence_date"), blank=True)
+    remaining_lease = models.CharField(_("remaining_lease"), max_length=255)
+    resale_price = models.IntegerField(_("resale_price"), blank=True)
+    
+    def __str__(self) -> str:
+        return f"{self.id} {self.month} {self.town} {self.flat_type} {self.block} {self.street_name} {self.storey_range} {self.floor_area_sqm} {self.flat_model} {self.lease_commence_date} {self.remaining_lease} {self.resale_price}"
+    
 # with urllib.request.urlopen(link) as url:
 #     fileobj = urllib.urlopen(url)
 #     print(fileobj.read())
