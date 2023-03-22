7a8d02b47a23db009eacde3963d6c69e88c1b154
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-03-22 11:42:00 +0800

    Added Paginator

--- jfcbackend/pricePrediction/views.py
+++ jfcbackend/pricePrediction/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import HousePrice
+from django.core.paginator import Paginator
 
 import requests
 # Create your views here.
@@ -8,4 +9,15 @@ import requests
 # Price Prediction View According to Alphabetic Order
 def all_house_price(request):
     house_list = HousePrice.objects.all()
-    return render(request, 'all_house_price.html', {'house_list': house_list})
+
+    house_paginator = Paginator(house_list, 100)
+
+    page_num = request.GET.get('page')
+
+    page = house_paginator.get_page(page_num)
+
+    context = {
+        'count': house_paginator.count,
+        'page': page
+        }
+    return render(request, 'all_house_price.html', context)
