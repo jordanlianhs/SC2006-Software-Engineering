6915575cf37e121bfe845a15d44fee134a8089f2
Xiaoyany88 <104213111+Xiaoyany88@users.noreply.github.com>
2023-03-24 01:25:02 +0800

    listing detail done

--- jfcproperty/src/components/listing-details/index.jsx
+++ jfcproperty/src/components/listing-details/index.jsx
@@ -26,24 +26,6 @@ const index = () => {
         </div>
       </section>
 
-      {/* <!-- Agent Single Grid View --> */}
-      <section className="our-agent-single bgc-f7 pb30-991">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 col-lg-8">
-              <DetailsContent />
-            </div>
-            {/* End details content .col-lg-8 */}
-
-            <div className="col-lg-4 col-xl-4">
-              <Sidebar />
-            </div>
-            {/* End sidebar content .col-lg-4 */}
-          </div>
-          {/* End .row */}
-        </div>
-      </section>
-
       {/* <!-- Our Footer --> */}
       <section className="footer_one">
         <div className="container">
---
+++ jfcproperty/src/pages/listing-details-v1/[id].js
@@ -0,0 +1,77 @@
+// pages/listing-details-v1/[id].js
+import React from "react";
+import { useRouter } from "next/router";
+import allResaleFlats from "../../data/properties";
+import Header from "../../components/common/header/DefaultHeader";
+import Footer from "../../components/common/footer/Footer";
+import CopyrightFooter from "../../components/common/footer/CopyrightFooter";
+
+const ListingDetails = ({ flat }) => {
+  if (!flat) {
+    return <div>Loading...</div>;
+  }
+
+  // Render the listing details using the flat data
+  return (
+    <>
+      <Header />
+      {/* Render the details of the flat here */}
+      <section className="listing-title-area mt85 md-mt0">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="mb-3">{`${flat.blockNumber} ${flat.streetName}`}</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                <strong>Flat Type:</strong> {flat.flatType}
+              </p>
+              <p>
+                <strong>Price:</strong> ${flat.price}
+              </p>
+            </div>
+            <div className="col-md-6">
+              <img
+                src={flat.img}
+                alt={`${flat.blockNumber} ${flat.streetName}`}
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Our Footer --> */}
+      <section className="footer_one">
+        <div className="container">
+          <div className="row">
+            <Footer />
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Our Footer Bottom Area --> */}
+      <section className="footer_middle_area pt40 pb40">
+        <div className="container">
+          <CopyrightFooter />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export async function getServerSideProps({ params }) {
+  const flats = await allResaleFlats();
+  const flat = flats.find((flat) => flat.id === parseInt(params.id, 10));
+
+  return {
+    props: {
+      flat,
+    },
+  };
+}
+
+export default ListingDetails;
+
