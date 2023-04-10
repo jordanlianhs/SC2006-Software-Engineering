c6de6dbea5b1ed6abcc02fd9dbc8d81a68e392e9
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-04-11 00:06:37 +0800

    Deleted findhouse and jfc

--- findhouse/src/features/api/api.js
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-export const api = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000/api",
-    }),
-    endpoints: (builder) => ({}),
-});
